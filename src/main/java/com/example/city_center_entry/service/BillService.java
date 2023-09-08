0012e609890448424e4ce86d72257cf9b530ce98
Artem Peshkov <a.peshkov@fstan.ru>
2023-09-08 17:13:41 +0200

    add bill as JSON

--- src/main/java/com/example/city_center_entry/service/BillService.java
+++ src/main/java/com/example/city_center_entry/service/BillService.java
@@ -2,10 +2,13 @@ package com.example.city_center_entry.service;
 
 import com.example.city_center_entry.entity.Bill;
 import com.example.city_center_entry.entity.Car;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class BillService {
     //@Inject bean be here
     DataService dataService;
@@ -13,6 +16,7 @@ public class BillService {
     //@Inject bean be here
     NotificationService notificationService;
 
+    @GetMapping
     public List<Bill> createAndSendBill(Car car) {
         List<Bill> billList = new ArrayList<>();
         for (String d : car.getAimOfEntry()) {
