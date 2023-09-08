0b7fc852c3821d060d98b6362ebb992e04e0993e
Artem Peshkov <a.peshkov@fstan.ru>
2023-09-08 17:33:29 +0200

    add bill as JSON

--- src/main/java/com/example/city_center_entry/service/BillService.java
+++ src/main/java/com/example/city_center_entry/service/BillService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@RestController("/bills")
 public class BillService {
     //@Inject bean be here
     DataService dataService;
