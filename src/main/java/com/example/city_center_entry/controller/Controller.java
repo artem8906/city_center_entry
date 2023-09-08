c9b468111637d49577fc159dc66c9a4744cf60c6
Tadeáš Kapina <tadeas.kapina4@gmail.com>
2023-09-08 13:19:10 +0200

    pridany lombok, merge

--- src/main/java/com/example/city_center_entry/controller/Controller.java
+++ src/main/java/com/example/city_center_entry/controller/Controller.java
@@ -1,22 +1,16 @@
 package com.example.city_center_entry.controller;
 
 import com.example.city_center_entry.entity.Auto;
-import com.example.city_center_entry.service.DataService;
 import com.example.city_center_entry.service.NotificationService;
-import org.springframework.web.bind.annotation.*;
 import com.example.city_center_entry.service.RequestService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/")
 public class Controller {
 
-//    @Inject bean here
+    //    @Inject bean here
     RequestService requestService;
-
 
 
     //    @Inject bean here
@@ -50,12 +44,4 @@ public class Controller {
         return "main";
     }
 
-    //level of security - admin
-    //pre admina
-    //pridava vela aut naraz bez schvalenia na zaklade range of ECV (relevantno pre sanitki, policajtov a td)
-    @PostMapping("/addFewCars")
-    public String addFewCarsByAdmin(String rangeOfLicensePlate, String owner) {
-        dataService.addFewCars(Auto.createFewAutos(rangeOfLicensePlate, owner));
-        return "admin";
-    }
 }
--- src/main/java/com/example/city_center_entry/controller/SpravcaProfilController.java
+++ src/main/java/com/example/city_center_entry/controller/SpravcaProfilController.java
@@ -1,4 +1,7 @@
 package com.example.city_center_entry.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class SpravcaProfilController {
 }
